f95c9527b555778fe4ead289c7251f2c1caaac81
Vikash <kvikash@apac.corpdir.net>
2019-09-24 18:41:27 +0530

    Camera PWA POC

---
+++ scripts/upload.js
@@ -0,0 +1,67 @@
+window.onload = function() {
+    //Check File API support
+    if (window.File && window.FileList && window.FileReader) {
+        $('#files').on("change", function(event) {
+            var files = event.target.files; //FileList object
+            var output = document.getElementById("result");
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                //Only pics
+                // if(!file.type.match('image'))
+                if (file.type.match('image.*')) {
+                    //set an upload limit - Can be whatever
+                    if (this.files[0].size < 20971520) {
+                        // continue;
+                        var picReader = new FileReader();
+                        picReader.addEventListener("load", function(event) {
+                            var picFile = event.target;
+                            var div = document.createElement("div");
+                            div.className = 'image-container';
+                            div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
+                                "title='preview image'/><i class='material-icons'>clear</i>";
+                            output.insertBefore(div, null);
+                            //remove an image from the list when you click/tap on it
+                            $('.image-container i').on("click", function() {
+                                $(this).parent().remove();
+                            });
+
+
+                            var img = $('.image-container');
+
+                            $(img, this).click(function() {
+                                console.log("image clicked");
+                                var modal = $('#myModal');
+                                $(modal).css("display", "block")
+                            });
+                        });
+                        //Read the image
+                        $('#clear, #result').show();
+                        picReader.readAsDataURL(file);
+                    } else {
+                        alert("Image Size is too big. Minimum size is 2MB.");
+                        $(this).val("");
+                    }
+                } else {
+                    alert("You can only upload image file.");
+                    $(this).val("");
+                }
+            }
+
+        });
+    } else {
+        console.log("Your browser does not support File API");
+    }
+}
+
+$('#files').on("click", function() {
+    $('.thumbnail').parent().remove();
+    $('result').hide();
+    $(this).val("");
+});
+
+$('#clear').on("click", function() {
+    $('.thumbnail').parent().remove();
+    $('#result').hide();
+    $('#files').val("");
+    $(this).hide();
+});
